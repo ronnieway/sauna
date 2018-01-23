4e0d422f58bd883cc1b145e648425eb06d6690e0
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-01-23 16:31:44 +0200

    checking view on ios (33)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -66,7 +66,7 @@ export class HomePage {
 
           browser.executeScript({
             code: `let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-                  if (iOS){
+                  if (iOS && event.url !== 'http://10.0.1.15/syswww/login.xml'){
                     (function(){
                       let table = document.querySelector('body');
                       let wWidth = window.screen.width;
@@ -82,7 +82,7 @@ export class HomePage {
                       } else {
                         body.style.cssText += '; transform:scale(' + vRatio + ');';
                       }
-                      body.style.cssText += '; transform:translate(0,0);';
+
                     })();
                   };`
           });
