25fa56881a81cd7300f4032f0f11c9bf050f6443
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-01-30 13:56:20 +0200

    again ios 29

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -127,11 +127,11 @@ export class HomePage {
                     wrapper.style.paddingLeft = '0px';
                     
                     if (vRatio >= hRatio) { 
-                      wrapper.style.cssText += '; transform:scale(' + hRatio + ');';
+                      wrapper.style.cssText += '; -webkit-transform:scale(' + hRatio + '); transform:scale(' + hRatio + ');';
 
                       a = (window.innerWidth - wrapper.offsetWidth * hRatio)/2;
                     } else {
-                      wrapper.style.cssText += '; transform:scale(' + vRatio + ');';
+                      wrapper.style.cssText += '; -webkit-transform:scale(' + vRatio + '); transform:scale(' + vRatio + ');';
 
                       a = (window.innerWidth - wrapper.offsetWidth * vRatio)/2;
                     }
