83001684d0214999a8ec3a6ac3e7cc5d85b95878
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-01-30 13:48:31 +0200

    again ios 28

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -89,6 +89,7 @@ export class HomePage {
                   margin: 0 auto !important;
                   width: 100%;
                   height: 100%;
+                  table-layout: auto !important;
                 }
                 html{
                   overflow: hidden !important;
