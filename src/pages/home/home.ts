cbeab0dab0aac68a26116ad3b78309f92cae9b42
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-01-30 02:22:17 +0200

    revert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { LoadingController, AlertController } from 'ionic-angular';
 import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
+
 
 @Component({
   selector: 'page-home',
@@ -16,14 +16,14 @@ export class HomePage {
 
   constructor(private iab: InAppBrowser,
               private loadingCtrl: LoadingController,
-              private alertCtrl: AlertController,
-              private screenOrientation: ScreenOrientation
+              private alertCtrl: AlertController
   ) {
   }
 
   public submitRequest() {
     this.connecting = true;
     this.theLoading();
+    this.loader.present();
     const options: InAppBrowserOptions = {
       zoom:'no',
       location:'no',
@@ -32,33 +32,32 @@ export class HomePage {
     };
     let browser =  this.iab.create(this.url, '_blank', options);
     browser.on('loadstart')
-    .subscribe(
-      (event) => {
-        setTimeout(() => {
-          if(this.connecting){
-            if(this.loader) {
-              this.loader.dismiss();
-              this.loader = null;
+      .subscribe(
+        (event) => {
+          setTimeout(() => {
+            if(this.connecting){
+              if(this.loader) {
+                this.loader.dismiss();
+              }
+              this.connecting = false;
+              this.presentAlert();
+              browser.close();
             }
-            this.connecting = false;
-            this.presentAlert();
-            browser.close();
-          }
-        }, 30000);
-      }
-    );
+          }, 30000);
+        }
+      );
 
     browser.on('loadstop')
-    .subscribe(
-      (event) => {
-        browser.executeScript({
-          code: `localStorage.setItem('iab', 'true');
+      .subscribe(
+        (event) => {
+          browser.executeScript({
+            code: `localStorage.setItem('iab', 'true');
                 (function() {
                   let body = document.querySelector('body');
                   let outerButton = document.createElement('div');
                   outerButton.setAttribute('id', 'outerBrowserButton');
                   let button = document.createElement('div');
-                  button.innerHTML = '< back';
+                  button.innerHTML = '<< back to connect';
                   button.setAttribute('id', 'closeBrowserButton');
                   button.onclick = function() { 
                     localStorage.setItem('iab', 'false'); 
@@ -66,215 +65,175 @@ export class HomePage {
                   outerButton.appendChild(button);
                   body.appendChild(outerButton);
                 })();`
-        });
+          });
 
-        browser.insertCSS({
-          code: `body{
+          let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+          if (iOS){
+            browser.insertCSS({
+              code: `body{;
                   margin: 0 !important;
-                  padding-top: 0;
+                  padding-top: 30px;
                 }
                 #customWrapper{
                   margin: 0 auto !important;
                   width: 803px !important;
                 }
-                #pmess{
-                  margin-top: 90px;
-                }
                 #wrapper.border{
-                  width: 420px !important;
+                  width: 480px !important;
                   margin: 0 auto;
+                  margin-bottom: 10px;
                 }
                 table{
                   margin: 0 auto !important;
-                  width: 100%;
-                  height: 100%;
                 }
                 html{
                   overflow: hidden !important;
                 }`
-        });
+            });
 
-        let iOS = true; // /iPad|iPhone|iPod/.test(navigator.userAgent);
-        if (iOS){
-          browser.executeScript({
-            code: `let wrapper = document.createElement("div");
+            browser.executeScript({
+              code: `let wrapper = document.createElement("div");
                   wrapper.id = "customWrapper";
+                  
                   while (document.body.firstChild)
                   {
                       wrapper.appendChild(document.body.firstChild);
                   }
-                  document.body.appendChild(wrapper);
                   
-                  window.addEventListener('touchmove', (e) => {
+                  document.body.appendChild(wrapper);
+                  let connectButton = document.getElementById('outerBrowserButton');
+                  
+                  let a;
+                  let wHeight;
+                  let wWidth;
+                  let absWidth = 803;
+                  let absHeight = 600;
+                  let vRatio;
+                  let hRatio;
+                  
+                  function zoomIt(){
+                    wHeight = window.screen.height;
+                    wWidth = window.screen.width;
+ 
+                    vRatio = wHeight / absHeight;
+                    hRatio = wWidth / absWidth;
+                    
+
+                    wrapper.style.paddingLeft = '0px';
+                    
+                    if (vRatio >= hRatio) { 
+                      wrapper.style.cssText += '; transform:scale(' + hRatio + ');';
+                      a = (window.innerWidth - wrapper.offsetWidth * hRatio)/(2 * hRatio);
+                    } else {
+                      wrapper.style.cssText += '; transform:scale(' + vRatio + ');';
+                      a = (window.innerWidth - wrapper.offsetWidth * vRatio)/(2 * vRatio);
+                    }
+                    wrapper.style.transformOrigin = 'left top';
+
+                    wrapper.style.paddingLeft =  a +  'px';
+                    wrapper.style.paddingTop = '30px';
+                    connectButton.style.left = '0px';
+                      
+                    
+                  };
+                  zoomIt();
+                  
+                  window.addEventListener('touchmove', function(e) {
                     e.preventDefault();
                   }, false);
-
-                  window.addEventListener('scroll', (e) => {
+                  
+                  window.addEventListener('scroll', function(e) {
                     e.preventDefault();
                   }, false);
-
-                   window.addEventListener('dragstart', (e) => {
-                    e.preventDefault();
-                  }, false);`
-          })
-        }
-
-        let portraitCode =`let a;
-          let wWidth;
-          let absWidth = 803;
-          let hRatio;
-          
-          function zoomIt(){
-            wWidth = window.screen.width;
-            hRatio = wWidth / absWidth;
-            wrapper.style.paddingLeft = '0px';
-
-            wrapper.style.transform = 'scale(' + hRatio + ')';
-            a = 0;
-            wrapper.style.transformOrigin = 'left top';
-            alert (wWidth + ' ' + hRatio + ' ' + a);
-            wrapper.style.paddingTop = '0px';
+                  
+                  window.addEventListener('orientationchange', zoomIt);`
+            });
           };
-          zoomIt();`;
-
-        let landscapeCode =`let b;
-          let wHeight;
-          let absHeight = 600;
-          let vRatio;
-
-          function zoomIt(){
-            wHeight = window.screen.height;
-
-            vRatio = wHeight / absHeight;
-            wrapper.style.paddingLeft = '0px';
-
-            wrapper.style.transform = 'scale(' + vRatio + ')';
-            b = (window.screen.width - wrapper.offsetWidth * vRatio) / 2;
-            wrapper.style.transformOrigin = 'left top';
-            alert (wHeight + ' ' + vRatio + ' ' + b);
-            wrapper.style.paddingTop = '0px';
-          };
-          zoomIt();`;
-
-        if (this.screenOrientation.type.includes('portrait')) {
-          alert('1 ' + this.screenOrientation.type);
-          browser.executeScript({
-            code: portraitCode
-          });
-        } else if (this.screenOrientation.type.includes('landscape')) {
-          alert('2 ' + this.screenOrientation.type);
-          browser.executeScript({
-            code: landscapeCode
-          });
-        } else {
-          console.log('something wrong');
-        }
-
-        this.screenOrientation.onChange()
-          .subscribe(
-          () => {
-            alert('3 ' + this.screenOrientation.type);
-            if (this.screenOrientation.type.includes('portrait')) {
-              browser.executeScript({
-                code: portraitCode
-              });
-            } else if (this.screenOrientation.type.includes('landscape')) {
-              browser.executeScript({
-                code: landscapeCode
-              });
-            } else {
-              console.log('something wrong');
-            }
-          }
-        );
-
-        browser.insertCSS({
-          code: `table, tbody, td, tr{
+          browser.insertCSS({
+            code: `body{
+                  
+                }
+                table, tbody, td, tr{
                   border: 0 !important;
                 }
                 #closeBrowserButton{
-                  padding: 10px 0 4px 20px;
+                  padding: 5px 0 5px 20px;
                   margin: 0 auto;
                   position: relative;
-                  color: blue;
+                  color: blue; 
                   font-size: 4vh;
                   font-weight: bold;
                   text-align: left;
-                  line-height: 4vh;
+                  line-height: 4vh; 
                   z-index: 99999;
                   width: 100%;
                 }
-                #outerBrowserButton{
+                #outerBrowserButton{ 
                   position: fixed;
                   left: 0;
                   float: left;
                   top: 0;
                   width: 803px;
-                  z-index: 99998;
+                  height: 40px;
+                  margin-top: 10px;
                 }`
-        });
+          });
 
-        let checkingClick = setInterval(() => {
-          browser.executeScript({
-            code: `let result = function(){
+          let checkingClick = setInterval(() => {
+            browser.executeScript({
+              code: `let result = function(){
                     return localStorage.iab;
                   };
                   result();`
             })
-            .then(val =>{
-              if (val[0] == 'false'){
-                clearInterval(checkingClick);
-                browser.close();
-              }
-            });
-        }, 1000);
+              .then(val =>{
+                if (val[0] == 'false'){
+                  clearInterval(checkingClick);
+                  browser.close();
+                }
+              });
+          }, 1000);
 
-        if(this.connecting) {
-          if (this.loader) {
-            this.loader.dismiss();
-            this.loader = null;
+          if(this.connecting) {
+            if (this.loader) {
+              this.loader.dismiss();
+            }
+            this.connecting = false;
+            browser.show();
           }
-          this.connecting = false;
-          browser.show();
-        }
-      },
-      (err) => {
-        if(this.connecting) {
-          if(this.loader) {
-            this.loader.dismiss();
-            this.loader = null;
+        },
+        (err) => {
+          if(this.connecting) {
+            if(this.loader) {
+              this.loader.dismiss();
+            }
+            this.connecting = false;
+            this.presentAlert();
+            browser.close();
           }
-          this.connecting = false;
-          this.presentAlert();
-          browser.close();
         }
-      }
-    );
+      );
 
     browser.on("loaderror")
-    .subscribe(
-      (err) => {
-        if (this.connecting) {
-          if (this.loader) {
-            this.loader.dismiss();
-            this.loader = null;
+      .subscribe(
+        (err) => {
+          if (this.connecting) {
+            if (this.loader) {
+              this.loader.dismiss();
+            }
+            this.connecting = false;
+            this.presentAlert();
+            browser.close();
           }
-          this.connecting = false;
-          this.presentAlert();
-          browser.close();
         }
-      }
-    );
+      );
   }
 
   theLoading() {
-    if(!this.loader) {
-      this.loader = this.loadingCtrl.create({
-        content: 'Connecting to server...',
-        duration: 30000
-      });
-      this.loader.present();
-    }
+    this.loader = this.loadingCtrl.create({
+      content: 'Connecting to server...',
+      duration: 30000
+    });
   }
 
   presentAlert() {
