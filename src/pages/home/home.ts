1927e99455c4e60fd6eb5d8e7775939f0acb3cdd
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-01-30 12:01:50 +0200

    again ios 15

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -159,7 +159,7 @@ export class HomePage {
                   border: 0 !important;
                 }
                 #closeBrowserButton{
-                  padding: 2px 0 2px 50px;
+                  padding: 0 0 0 50px;
                   margin: 0 auto;
                   position: relative;
                   color: white; 
