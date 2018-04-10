516058db9b86e0f8adb1f91786aa28f94ee5e24d
Ruslan Dorozhovets <rrruslan@yahoo.com>
2018-04-11 00:34:01 +0200

    check viewport 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -113,6 +113,11 @@ export class HomePage {
                       e.preventDefault(); 
                     }, false);
                     
+                    var meta = document.createElement('meta');
+                    meta.name = 'viewport';
+                    meta.content = 'viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no';
+                    document.getElementsByTagName('head')[0].appendChild(meta);
+      
                     window.addEventListener('orientationchange', zoomIt);`
             });
           } else {
